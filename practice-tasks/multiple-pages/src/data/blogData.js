3d41ca3f285a3b98b2dd848b861f0caf5f96a12e
Mavia-Ahmed <maviaahmed055@gmail.com>
2025-08-08 16:41:31 +0500

    Add files via upload

---
+++ practice-tasks/multiple-pages/src/data/blogData.js
@@ -0,0 +1,34 @@
+// src/data/blogData.js
+import blog1Image from '../assets/blog1.webp';
+
+const blogData = [
+  {
+    id: 1,
+    title: "First Blog Post",
+    time: "Posted on July 30, 2025 by Admin",
+    image: blog1Image,
+    para: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempus sapien et lorem porttitor, ac egestas leo vestibulum. Suspendisse potenti. Cras ac magna vitae arcu sollicitudin congue. Sed vel imperdiet quam. Nullam vitae fermentum magna. Aliquam erat volutpat. Cras ac magna vitae arcu sollicitudin congue. Sed vel imperdiet quam. Nullam vitae fermentum magna. Aliquam erat volutpat.",
+    summary: "This is the summary of the first blog post.",
+    link: "/post1"
+  },
+  {
+    id: 2,
+    title: "Second Blog Post",
+    time: "Posted on July 30, 2025 by Admin",
+    image: blog1Image,
+    para: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempus sapien et lorem porttitor, ac egestas leo vestibulum. Suspendisse potenti. Cras ac magna vitae arcu sollicitudin congue. Sed vel imperdiet quam. Nullam vitae fermentum magna. Aliquam erat volutpat. Cras ac magna vitae arcu sollicitudin congue. Sed vel imperdiet quam. Nullam vitae fermentum magna. Aliquam erat volutpat.",
+    summary: "This is the summary of the second blog post.",
+    link: "/post1"
+  },
+  {
+    id: 3,
+    title: "Second Blog Post",
+    time: "Posted on July 30, 2025 by Admin",
+    image: blog1Image,
+    para: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempus sapien et lorem porttitor, ac egestas leo vestibulum. Suspendisse potenti. Cras ac magna vitae arcu sollicitudin congue. Sed vel imperdiet quam. Nullam vitae fermentum magna. Aliquam erat volutpat. Cras ac magna vitae arcu sollicitudin congue. Sed vel imperdiet quam. Nullam vitae fermentum magna. Aliquam erat volutpat.",
+    summary: "This is the summary of the second blog post.",
+    link: "/post1"
+  }
+];
+
+export default blogData;
