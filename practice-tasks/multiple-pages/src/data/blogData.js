bb841d4559e82d0952a20bf7d724d64898e41602
Mavia-Ahmed <maviaahmed055@gmail.com>
2025-08-12 01:30:27 +0500

    blogData.js

--- practice-tasks/multiple-pages/src/data/blogData.js
+++ practice-tasks/multiple-pages/src/data/blogData.js
@@ -1,5 +1,7 @@
 // src/data/blogData.js
 import blog1Image from '../assets/blog1.webp';
+import blog2Image from '../assets/blog2.jpg';
+import blog3Image from '../assets/blog3.webp';
 
 const blogData = [
   {
@@ -15,20 +17,21 @@ const blogData = [
     id: 2,
     title: "Second Blog Post",
     time: "Posted on July 30, 2025 by Admin",
-    image: blog1Image,
+    image: blog2Image,
     para: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempus sapien et lorem porttitor, ac egestas leo vestibulum. Suspendisse potenti. Cras ac magna vitae arcu sollicitudin congue. Sed vel imperdiet quam. Nullam vitae fermentum magna. Aliquam erat volutpat. Cras ac magna vitae arcu sollicitudin congue. Sed vel imperdiet quam. Nullam vitae fermentum magna. Aliquam erat volutpat.",
     summary: "This is the summary of the second blog post.",
-    link: "/post1"
+    link: "/post2"
   },
   {
     id: 3,
     title: "Second Blog Post",
     time: "Posted on July 30, 2025 by Admin",
-    image: blog1Image,
+    image: blog3Image,
     para: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempus sapien et lorem porttitor, ac egestas leo vestibulum. Suspendisse potenti. Cras ac magna vitae arcu sollicitudin congue. Sed vel imperdiet quam. Nullam vitae fermentum magna. Aliquam erat volutpat. Cras ac magna vitae arcu sollicitudin congue. Sed vel imperdiet quam. Nullam vitae fermentum magna. Aliquam erat volutpat.",
     summary: "This is the summary of the second blog post.",
-    link: "/post1"
+    link: "/post3"
   }
 ];
 
 export default blogData;
+
