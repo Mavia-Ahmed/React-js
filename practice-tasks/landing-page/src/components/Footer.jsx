fafa4315552f42508475ac79c7ecaae6f665294c
Mavia-Ahmed <maviaahmed055@gmail.com>
2025-08-11 13:29:04 +0500

    Footer.jsx

--- practice-tasks/landing-page/src/components/Footer.jsx
+++ practice-tasks/landing-page/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Container } from 'react-bootstrap';
 
 function Footer() {
   return (
-    <footer className="bg-dark text-light py-4 mt-5">
+    <footer className="bg-dark text-light py-2 mt-3">
       <Container className="text-center">
         <p>© 2025 Khanestate. All rights reserved.</p>
       </Container>
@@ -11,3 +11,4 @@ function Footer() {
 }
 
 export default Footer;
+
