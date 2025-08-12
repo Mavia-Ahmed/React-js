b4373dcb6019e40d118d2ac861e971cba2bad0b4
Mavia-Ahmed <maviaahmed055@gmail.com>
2025-08-12 18:26:47 +0500

    Navbar.jsx

--- practice-tasks/multiple-pages/src/components/Navbar.jsx
+++ practice-tasks/multiple-pages/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
+import logoWhite from './src/assets/logo-white.png';
 
 const Navbar = () => {
   return (
     <nav>
-      <div className="logo"><img style={{filter: "brightness(0) invert(1)", height: "30px", width: "60px", marginBottom: "-7px"}} src="./src/assets/logo-white.png" alt=""  /><span>Khan estate</span></div>
+      <div className="logo"><img style={{filter: "brightness(0) invert(1)", height: "30px", width: "60px", marginBottom: "-7px"}} src={logoWhite} alt=""  /><span>Khan estate</span></div>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
@@ -16,3 +17,4 @@ const Navbar = () => {
 
 
 export default Navbar;
+
