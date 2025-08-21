5deb3ccef468dabfddaf38ca1d83e18ce2889a79
Mavia-Ahmed <maviaahmed055@gmail.com>
2025-08-21 23:05:13 +0500

    Add files via upload

---
+++ learning-journey/01_components/simple-component/Student.jsx
@@ -0,0 +1,11 @@
+function Student(){
+    return (
+        <div>
+        <h3>Blog 1</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis eos ipsam ducimus animi eligendi molestias ipsum delectus, nam quam natus voluptate dolorum dolore, illum laborum deleniti eaque aliquid vel doloribus?</p>
+        <p>2025-08-21</p>
+        </div>
+    )
+}
+
+export default Student;
