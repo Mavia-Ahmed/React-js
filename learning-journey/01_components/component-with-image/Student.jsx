66d54afa363eaf1c96d7bc47f94fe37201b4f61e
Mavia-Ahmed <maviaahmed055@gmail.com>
2025-08-21 23:07:20 +0500

    Add files via upload

---
+++ learning-journey/01_components/component-with-image/Student.jsx
@@ -0,0 +1,14 @@
+import Image1 from '../public/blog1.webp'
+
+function Student(){
+    return (
+        <div>
+        <img src={Image1} width="100px" height="100"/>
+        <h3>Blog 1</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis eos ipsam ducimus animi eligendi molestias ipsum delectus, nam quam natus voluptate dolorum dolore, illum laborum deleniti eaque aliquid vel doloribus?</p>
+        <p>2025-08-21</p>
+        </div>
+    )
+}
+
+export default Student;
