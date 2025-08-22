8f635207664c35f5a6ae0a8694943fe4441ec66c
Mavia-Ahmed <maviaahmed055@gmail.com>
2025-08-22 22:01:34 +0500

    data.js

---
+++ learning-journey/02_props/type3-datafile-array/data.js
@@ -0,0 +1,19 @@
+import Image1 from '../public/blog1.webp'
+import Image2 from '../public/blog2.png'
+
+const blog = [
+    {
+        image: Image1,
+        head: "Blog 1",
+        description: "Lorem ispsum wala option agan nahi aaya yahan ab meri koi ghalti nahi ismein",
+        date: "2025-08-22"
+    },
+    {
+        image: Image2,
+        head: "Blog 2",
+        description: "yeh wali description alag blog ki hai",
+        date: "2025-08-25"
+    }
+]
+
+export default blog;
