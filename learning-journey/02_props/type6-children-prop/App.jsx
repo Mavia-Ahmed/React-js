59d928ab63248558f2d25eeaede1c03049c72a10
Mavia-Ahmed <maviaahmed055@gmail.com>
2025-08-22 22:26:16 +0500

    App.jsx

---
+++ learning-journey/02_props/type6-children-prop/App.jsx
@@ -0,0 +1,14 @@
+import Card from './Card'
+
+
+function App() {
+    return (
+        <div>
+            <card>
+                <h3>Blog 1</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam cum vero pariatur ut ipsa dolorum eveniet a, eius</p>
+            </card>
+        </div>
+    );
+}
+export default App;
