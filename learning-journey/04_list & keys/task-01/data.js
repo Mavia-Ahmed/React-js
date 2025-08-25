8771c505993e3a129175f967fc1d6c3a00f52e29
Mavia-Ahmed <maviaahmed055@gmail.com>
2025-08-25 20:52:29 +0500

    data.js

---
+++ learning-journey/04_list & keys/task-01/data.js
@@ -0,0 +1,28 @@
+const data = [
+        {
+            id: 1,
+            rollNo: "123",
+            name: "khan",
+            age: "19",
+            group: "Commerce",
+            status: true
+        },
+        {
+            id: 2,
+            rollNo: "456",
+            name: "David",
+            age: "25",
+            group: "Arts",
+            status: true
+        },
+        {
+            id: 3,
+            rollNo: "789",
+            name: "Ali",
+            age: "22",
+            group: "Science",
+            status: false
+        }
+    ]
+
+export default data;
