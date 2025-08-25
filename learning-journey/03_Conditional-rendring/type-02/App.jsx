5908d9301bd72005978e405df504404d9ec1c826
Mavia-Ahmed <maviaahmed055@gmail.com>
2025-08-25 14:16:37 +0500

    App.jsx

---
+++ learning-journey/03_Conditional-rendring/type-02/App.jsx
@@ -0,0 +1,24 @@
+//TYPE-2 PART 1     ternary operator
+import Student from './Student'
+
+function App() {
+    return (
+        <div>
+            <Student marks="31"/>
+        </div>
+    )
+}
+
+export default App;
+
+
+//TYPE-2 PART 2      Logical AND (&&)
+import Student from './Student'
+
+function App(){
+    return (
+        <Student hasCertificate={true}/>
+    )
+}
+
+export default App;
