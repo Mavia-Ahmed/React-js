8cf03878657ccb186edf3f53ff2fa9845b95ceaf
Mavia-Ahmed <maviaahmed055@gmail.com>
2025-08-25 21:32:51 +0500

    data.js

---
+++ learning-journey/04_list & keys/task-02/data.js
@@ -0,0 +1,30 @@
+import Image1 from '../public/blog1.webp'
+import Image2 from '../public/blog2.jpg'
+import Image3 from '../public/blog3.jpeg'
+
+
+const house = [
+    {
+        id: 1,
+        head: "House 1",
+        image: Image1,
+        description: "The beautiful house of canada",
+        status: true
+    },
+    {
+        id: 2,
+        head: "House 2",
+        image: Image2,
+        description: "The beautiful house of london",
+        status: false
+    },
+    {
+        id: 3,
+        head: "House 3",
+        image: Image3,
+        description: "The beautiful house of italy",
+        status: true
+    }
+]
+
+export default house;
