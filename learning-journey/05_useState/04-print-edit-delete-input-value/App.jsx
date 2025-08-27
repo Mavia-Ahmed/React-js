9e07091e726e98dc145425ce456b191654e8b77d
Mavia-Ahmed <maviaahmed055@gmail.com>
2025-08-27 19:55:48 +0500

    App.jsx

--- learning-journey/05_useState/04-print-edit-delete-input-value/App.jsx
+++ learning-journey/05_useState/04-print-edit-delete-input-value/App.jsx
@@ -1,1 +1,30 @@
+import { useState } from 'react';
 
+function App(){
+    const [para, setPara] = useState("")
+    const [print, setPrint] = useState("")
+    function handleClick(){
+        setPrint(para);
+        setPara("");
+    }
+    function handleText(){
+        setPrint("");
+    }
+    function handleEdit(){
+        setPara(print);
+    }
+
+
+    return(
+        <div>
+            <input 
+            type="text" 
+            value={para}
+            onChange={(e)=>setPara(e.target.value)}
+            />
+            <button onClick={handleClick}>Click me</button><button onClick={handleText}>Delete</button><button onClick={handleEdit}>Edit</button>
+            <p>{print}</p>
+        </div>
+    )
+}
+export default App;
