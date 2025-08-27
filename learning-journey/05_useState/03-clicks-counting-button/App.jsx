087be90a010c6aa2d89a209ab9011adc866d73f8
Mavia-Ahmed <maviaahmed055@gmail.com>
2025-08-27 19:38:08 +0500

    App.jsx

--- learning-journey/05_useState/03-clicks-counting-button/App.jsx
+++ learning-journey/05_useState/03-clicks-counting-button/App.jsx
@@ -1,1 +1,14 @@
+import { useState } from 'react';
 
+function App(){
+    const [count, setCount] = useState(0)
+    function handleClick(){
+        setCount(count + 1)
+    }
+    return(
+        <div>
+            <button onClick={handleClick}>Count: {count}</button>
+        </div>
+    )
+}
+export default App;
