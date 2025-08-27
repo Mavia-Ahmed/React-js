3b41080d679b9b176a88c672e6ee7fc5ca97b653
Mavia-Ahmed <maviaahmed055@gmail.com>
2025-08-27 19:33:47 +0500

    App.jsx

--- learning-journey/05_useState/02-print-input-value/App.jsx
+++ learning-journey/05_useState/02-print-input-value/App.jsx
@@ -1,1 +1,23 @@
+import { useState } from 'react';
 
+function App(){
+    const [para, setPara] = useState("")
+    const [print, setPrint] = useState("")
+    function handleClick(){
+        setPrint(para)
+    }
+
+
+    return(
+        <div>
+            <input 
+            type="text" 
+            value={para}
+            onChange={(e)=>setPara(e.target.value)}
+            />
+            <button onClick={handleClick}>Click me</button>
+            <p>{print}</p>
+        </div>
+    )
+}
+export default App;
