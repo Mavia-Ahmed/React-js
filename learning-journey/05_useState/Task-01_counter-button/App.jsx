bf53f6814ac07ad02dbd0dab6dbd98615fd1043a
Mavia-Ahmed <maviaahmed055@gmail.com>
2025-08-27 19:51:25 +0500

    App.jsx

--- learning-journey/05_useState/Task-01_counter-button/App.jsx
+++ learning-journey/05_useState/Task-01_counter-button/App.jsx
@@ -1,1 +1,21 @@
-
+import { useState } from 'react';
+import './app.css'
+function App(){
+    const [count, setCount] = useState(0)
+    function handleClick(){
+        setCount(count + 1)
+    }
+    function changeNumber(){
+        if(count > 0){
+            setCount(count - 1)
+        }
+    }
+    return(
+        <div className="container">
+            <button onClick={changeNumber}>&lt;</button>
+            Count: {count}
+            <button onClick={handleClick}>&gt;</button>
+        </div>
+    )
+}
+export default App;
